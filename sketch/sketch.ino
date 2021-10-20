6ab05344dfb3b24a950cc76982baa6d893665d53
jumshudivanych <35367717+jumshudivanych@users.noreply.github.com>
2021-10-20 22:16:42 +0300

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -8,15 +8,17 @@
  * The URL here provides a hint to the browser about what page the user should
  * navigate to to interact with the device.
  */
-WebUSB WebUSBSerial(1 /* https:// */, "webusb.github.io/arduino/demos/rgb");
+WebUSB WebUSBSerial(1 /* https:// */, "jumshudivanych.github.io/test-technologies/");
 
 #define Serial WebUSBSerial
 
 const int redPin = 9;
 const int greenPin = 10;
 const int bluePin = 11;
+const int ledPin = 13;
 int color[3];
 int colorIndex;
+int ledStatus;
 
 void setup() {
   while (!Serial) {
@@ -26,10 +28,20 @@ void setup() {
   Serial.write("Sketch begins.\r\n");
   Serial.flush();
   colorIndex = 0;
+  
+  ledStatus = 0;
+  pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
   if (Serial && Serial.available()) {
+    ledStatus = Serial.read();
+    if (ledStatus == 0) {
+    digitalWrite(ledPin,LOW);
+    }
+    if (ledStatus == 1) {
+      digitalWrite(ledPin,HIGH);
+    }  
     color[colorIndex++] = Serial.read();
     if (colorIndex == 3) {
       analogWrite(redPin, color[0]);
